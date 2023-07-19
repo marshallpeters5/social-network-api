b71705406077ec99838a8b371ba068d4dbaacf6a
Marshall <marshall.peters01@gmail.com>
2023-07-18 23:29:34 -0400

    worked on routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,25 @@
+// Importing //
+const router = require('express').Router();
+const {
+  createUser, getUsers, getSingleUser, updateUser, deleteUser, addFriend, removeFriend, } 
+  = require('../../controllers/user-controller');
+
+// Routes for getting all users and creating a new user. //
+router.route('/')
+.post(createUser) // Create //
+.get(getUsers); // Get all //
+
+
+// Routes for getting a single user, updating a user, and deleting a user. //
+router.route('/:userId')
+.put(updateUser) // Update //
+.get(getSingleUser) // Get one //
+.delete(deleteUser); // Delete // 
+
+// Routes for adding and removing a friend for a specific user. //
+router.route('/:userId/friends/:friendId')
+.post(addFriend) // Post //
+.delete(removeFriend); // Delete //
+
+// Exporting //
+module.exports = router;
