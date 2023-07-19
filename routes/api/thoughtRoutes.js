75587d3c7ef88d7b44f1f656b3cc9e94f8c9a4e6
Marshall <marshall.peters01@gmail.com>
2023-07-18 23:30:13 -0400

    routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -4,22 +4,22 @@ const {
   getThoughts, getSingleThought, createThought, updateThought, deleteThought, addReaction, removeReaction, }
   = require('../../controllers/thought-controller');
 
-// Routes for getting all thoughts and creating a new thought.
+// Routes for getting all thoughts and creating a new thought. //
 router.route('/')
 .get(getThoughts) // Get all //
 .post(createThought); // Create //
 
-// Routes for getting a single thought, updating a thought, and deleting a thought.
+// Routes for getting a single thought, updating a thought, and deleting a thought. //
 router.route('/:thoughtId')
 .get(getSingleThought) // Get one //
 .put(updateThought) // Update //
 .delete(deleteThought); // Delete //
 
-// Route for adding a reaction to a specific thought.
+// Route for adding a reaction to a specific thought. //
 router.route('/:thoughtId/reactions')
 .post(addReaction); // Post //
 
-// Route for removing a specific reaction from a specific thought.
+// Route for removing a specific reaction from a specific thought. //
 router.route('/:thoughtId/reactions/:reactionId')
 .delete(removeReaction); // Delete //
 
